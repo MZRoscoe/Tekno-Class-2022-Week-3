00882e49b17ebd450469991718c93dfe9bbfa7d2
MZRoscoe <103299768+MZRoscoe@users.noreply.github.com>
2022-12-27 21:08:33 +0700

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -2,4 +2,17 @@ from dataset import data
 
 folder_id = int(input())
 
-# ----- Write your code here -----
+
+def find_folder(data, folder_id):
+    id_child = []
+    for i in data:
+        if i['id'] == folder_id:
+            for j in i['children']:
+                id_child.append(j['id'])
+        else:
+            if i['is_file'] == False:
+                id_child += find_folder(i['children'], folder_id)
+    return id_child
+
+print(find_folder(data, folder_id))
+
